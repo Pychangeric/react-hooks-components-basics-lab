7fa6688680dfc78820c66645e5429bcb7e866193
pychangeric <pychangeric@gmail.com>
2023-04-17 13:44:37 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -15,16 +16,22 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
+/* write an <About> component here */
+function About(){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
